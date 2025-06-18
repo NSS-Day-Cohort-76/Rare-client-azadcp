f82e0855061524deea6954fe14c407dc57d4bdb8
Kyle Mims <kylemims@gmail.com>
2025-06-18 13:16:37 -0500

    Created reusable components for the edit and delete icons, table for all posts. Used Bulma to get AllPostAdmin page rendered with table. Most elements are added to fulfill wireframe

--- src/components/posts/AllPostAdmin.jsx
+++ src/components/posts/AllPostAdmin.jsx
@@ -1,26 +1,40 @@
-import { useEffect, useState } from "react"
-import { GetAllPosts } from "../../managers/PostManager"
-
+import { useEffect, useState } from "react";
+import { GetAllPosts } from "../../managers/PostManager";
+import { PostTable } from "../shared/PostTable.js";
 
 export const AllPostAdmin = () => {
+  const [posts, setPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
 
-    const [posts, setPosts] = useState([])
-    useEffect(() => {
-        GetAllPosts().then(data => {
-        console.log("Fetched posts:", data)
-        setPosts(data)
-        })
-    }, [])
+  useEffect(() => {
+    GetAllPosts().then((data) => {
+      console.log("Fetched posts:", data);
+      setPosts(data);
+      setFilteredPosts(data);
+    });
+  }, []);
 
-    return (
-        <div>
-            <h2>Posts</h2>
-            {posts?.map((post)=>(
-                <div key={post.id} className="post-card">
-                    <h3>{post.title}</h3>
-                    <p></p>
-                </div>
-            ))}
-        </div>
-    )
-}
+  const handleSearch = (text) => {
+    const filtered = posts.filter((post) => post.title.toLowerCase().includes(text.toLowerCase()));
+    setFilteredPosts(filtered);
+  };
+
+  return (
+    <PostTable
+      posts={filteredPosts}
+      onEdit={(id) => console.log("Edit", id)}
+      onDelete={(id) => console.log("Delete", id)}
+      onSearch={handleSearch}
+    />
+  );
+};
+
+// <div>
+//   <h2>Posts</h2>
+//   {posts?.map((post) => (
+//     <div key={post.id} className="post-card">
+//       <h3>{post.title}</h3>
+//       <p></p>
+//     </div>
+//   ))}
+// </div>
