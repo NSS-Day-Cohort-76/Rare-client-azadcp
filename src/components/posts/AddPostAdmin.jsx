20c2e6774c49837c8b0b57865c85d3ddd48caccf
Kyle Mims <kylemims@gmail.com>
2025-06-24 13:18:49 -0500

    AddPostAdmin.jsx has completed structure. Making commit in prep for pulling down Jake's API updates. The newPost logic is currently being written and incomplete. Once the temporary newPost object is fully built, we can test a POST request using the POST request function in PostManager.js

--- src/components/posts/AddPostAdmin.jsx
+++ src/components/posts/AddPostAdmin.jsx
@@ -5,6 +5,21 @@ import { SharedInput } from "../shared/SharedInput.jsx";
 //* current USER should be recorded as  (first_name + last_name -> AS AUTHOR_NAME
 //* Redirect to the NEW Post details page (must fix MyPostAdmin routing issue)
 
+const newPost = {
+  userId: user_id,
+  category: category_id,
+  title: "",
+  publication_date: new Date().toISOString(),
+  image_url: "",
+  content: "",
+  approved: true,
+
+  // : tripName,
+  // userId: userObj.id,
+  // createdAt: new Date().toISOString(),
+  // parkId: parkId ? parseInt(parkId) : parseInt(selectedParkId),
+};
+
 export const AddPostAdmin = () => {
   return (
     <section className="section">
@@ -13,34 +28,45 @@ export const AddPostAdmin = () => {
           <span>
             <h1 className="title is-4 mb-4">New Post</h1>
           </span>
-          <SharedInput className="input is-normal mb-4 is-b" type={""} placeholder={"Title"} />
-          <SharedInput className="input is-normal" type={""} placeholder={"image URL"} />
         </div>
-        <div className="field">
-          <div className="control column is-two-thirds">
-            <textarea class="textarea is-medium" placeholder="Article Content"></textarea>
-          </div>
-          <div class="select is-normal">
-            <select>
-              <option>Select dropdown</option>
-              <option>With options</option>
-            </select>
-          </div>
-          <div class="checkboxes">
-            <label class="checkbox">
-              <input type="checkbox" />
-            </label>
-
-            <label class="checkbox">
-              <input type="checkbox" checked />
-            </label>
-
-            <label class="checkbox">
-              <input type="checkbox" checked />
-            </label>
+        <div className="field column is-half">
+          <div className="control">
+            <SharedInput className="input is-normal mb-4 is-b" type={""} placeholder={"Title"} />
+            <SharedInput className="input is-normal" type={""} placeholder={"image URL"} />
           </div>
         </div>
       </div>
+      <div className="field">
+        <div className="control column is-two-thirds">
+          <textarea className="textarea is-medium" placeholder="Article Content"></textarea>
+        </div>
+      </div>
+      <div className="field column is-half">
+        <div className="control">
+          <div className="select is-normal mb-4">
+            <select>
+              <option>Category Select</option>
+              <option>With options</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className="field column is three-quarters is-flex is-justify-content-space-between">
+        <div className="control">
+          <label class="checkbox">
+            <input type="checkbox" />
+          </label>
+
+          <label class="checkbox">
+            <input type="checkbox" />
+          </label>
+
+          <label class="checkbox">
+            <input type="checkbox" />
+          </label>
+        </div>
+      </div>
+      {/* </div> */}
     </section>
   );
 };
